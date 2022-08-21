298a34cf84f6ad8c5894cc7a860b09cdc3e2808e
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-08-21 19:52:52 +0300

    add three task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,7 +1,48 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
+//1ВАРИАНТ
+// const elements = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = ingredients[i];
+//   itemEl.classList.add("item");
+
+//   elements.push(itemEl);
+// }
+
+// console.log(elements);
+
+// const listEl = document.querySelector("#ingredients");
+
+// listEl.append(...elements);
+
+//2ВАРИАНТ
+// const listEl = document.querySelector("#ingredients");
+
+// const elements = ingredients.map((ingredient) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.textContent = ingredient;
+//   itemEl.classList.add("item");
+
+//   return itemEl;
+// });
+
+// listEl.append(...elements);
+
+//3ВАРИАНТ
+
 const listEl = document.querySelector("#ingredients");
 
-const itemEl = document.createElement("li");
-itemEl.classList.add("item");
-console.log(itemEl);
+const makeIngredients = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = ingredient;
+    itemEl.classList.add("item");
+
+    return itemEl;
+  });
+};
+
+const elements = makeIngredients(ingredients);
+listEl.append(...elements);
