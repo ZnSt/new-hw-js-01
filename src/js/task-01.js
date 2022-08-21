50b22cef399988708c4728519d33da8b92c9a46a
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-08-21 18:10:54 +0300

    first task done

---
+++ src/js/task-01.js
@@ -0,0 +1,21 @@
+const listEl = document.querySelector("#categories");
+console.log(listEl);
+
+const itemEl = document.querySelectorAll(".item");
+console.log(`Number of categories: `, itemEl.length);
+
+const titleEl = itemEl.querySelectorAll("h2");
+const newListEl = document.querySelectorAll(".item ul");
+for (let i = 0; i < titleEl.length; i += 1) {
+  console.log(`Category: `, titleEl[i].textContent);
+  console.log(`Elements: `, newListEl[i].children.length);
+}
+
+// const itemRefs = document.querySelectorAll(".item");
+// console.log("Number of categories: ", itemRefs.length);
+// itemRefs.forEach((element) => {
+//   const titleRefs = element.querySelector("h2").textContent;
+//   console.log("Category: ", titleRefs);
+//   const secondItemRefs = element.querySelectorAll("li");
+//   console.log("Elements: ", secondItemRefs.length);
+// });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,7 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const listEl = document.querySelector("#ingredients");
+
+const itemEl = document.createElement("li");
+itemEl.classList.add("item");
+console.log(itemEl);
