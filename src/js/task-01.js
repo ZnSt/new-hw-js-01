621b6cdf4fe948314b0238b777e422335107f467
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-08-23 16:51:27 +0300

    .

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,15 +1,15 @@
-const listEl = document.querySelector("#categories");
-console.log(listEl);
+// const listEl = document.querySelector("#categories");
+// console.log(listEl);
 
-const itemEl = document.querySelectorAll(".item");
-console.log(`Number of categories: `, itemEl.length);
+// const itemEl = document.querySelectorAll(".item");
+// console.log(`Number of categories: `, itemEl.length);
 
-const titleEl = itemEl.querySelectorAll("h2");
-const newListEl = document.querySelectorAll(".item ul");
-for (let i = 0; i < titleEl.length; i += 1) {
-  console.log(`Category: `, titleEl[i].textContent);
-  console.log(`Elements: `, newListEl[i].children.length);
-}
+// const titleEl = document.querySelectorAll("h2");
+// const newListEl = document.querySelectorAll(".item ul");
+// for (let i = 0; i < titleEl.length; i += 1) {
+//   console.log(`Category: `, titleEl[i].textContent);
+//   console.log(`Elements: `, newListEl[i].children.length);
+// }
 
 // const itemRefs = document.querySelectorAll(".item");
 // console.log("Number of categories: ", itemRefs.length);
