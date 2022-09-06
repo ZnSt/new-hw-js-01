d434d511a722ba3223889e9f22175894736485a9
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-06 14:32:56 +0300

    correct

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,29 +1,17 @@
-const refs = {
-  inputSizeControl: document.querySelector("#font-size-control"),
-  spanSizeControl: document.querySelector("#text"),
-};
+// const refs = {
+//   inputSizeControl: document.querySelector("#font-size-control"),
+//   spanSizeControl: document.querySelector("#text"),
+// };
 
-refs.inputSizeControl.addEventListener("input", onInputFn);
+// refs.inputSizeControl.addEventListener("input", onInputFn);
 
-function onInputFn(event) {
-  refs.spanSizeControl.style.fontSize = event.currentTarget.value + "px";
-}
+// function onInputFn(event) {
+//   refs.spanSizeControl.style.fontSize = event.currentTarget.value + "px";
+// }
 
-// Напиши функцію printContactsInfo(names, phones) яка виводить у консоль ім'я та телефонний номер користувача. У параметри names і phones будуть передані рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність. Кількість імен та телефонів гарантовано однакова
+const parent = document.querySelector("#parent");
 
-function printContactsInfo(names = "", phones = "") {
-  const namesArr = names.split(",");
-  const phonesArr = phones.split(",");
-
-  for (let i = 0; i < namesArr.length; i += 1) {
-    console.log(`${namesArr[i]} - ${phonesArr[i]}`);
-  }
-}
-
-printContactsInfo(
-  "Jacob, William, Solomon, Artemis",
-  "8900012345, 8976463721, 0987654321, 1234567891"
-);
-
-const obj = {};
-console.log(obj);
+parent.addEventListener("click", (event) => {
+  console.log("event.target: ", event.target);
+  console.log("event.currentTarget: ", event.currentTarget);
+});
