16db30e3ec342c3445c3fc6aac29d01b157404a0
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-10 14:13:50 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,35 +3,103 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
+// РЕФАКТОРИНГ
 const refs = {
-  divEl: document.querySelector("#boxes"),
-  inputEl: document.querySelector("[type='number']"),
-  createBtn: document.querySelector("[data-create]"),
-  destroyBtn: document.querySelector("[data-destroy]"),
+  input: document.querySelector("[type='number']"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestory: document.querySelector("[data-destroy]"),
+  div: document.querySelector("#controls"),
+  divBoxes: document.querySelector("#boxes"),
 };
 
-refs.createBtn.addEventListener("click", createBoxes);
-refs.destroyBtn.addEventListener("click", desctroyBoxes);
+refs.div.addEventListener("click", onClickHandler);
 
-function createBoxes() {
-  const amount = Number(refs.inputEl.value);
-  const newArr = [];
-  let counter = 30;
-  for (let i = 0; i < amount; i += 1) {
-    const newDiv = document.createElement("div");
-    newDiv.style.width = `${counter}px`;
-    newDiv.style.height = `${counter}px`;
-    newDiv.style.backgroundColor = getRandomHexColor();
+function onClickHandler(event) {
+  const btnEvent = event.target.dataset;
+  if (btnEvent.hasOwnProperty("create")) {
+    onBtnCreateClick();
+  } else if (btnEvent.hasOwnProperty("destroy")) {
+    onBtnDestroyClick();
+  } else {
+    return;
+  }
+}
 
-    counter += 10;
+function onBtnCreateClick() {
+  createBoxes(refs.input.value);
+  refs.input.value = "";
+}
 
-    newArr.push(newDiv);
+function onBtnDestroyClick() {
+  let { value } = refs.input;
+
+  if (!value || value < 0) {
+    refs.divBoxes.innerHTML = "";
+    return;
   }
 
-  refs.divEl.append(...newArr);
+  for (let i = 0; i < value; i++) {
+    if (i > refs.divBoxes.childElementCount - 1) {
+      return;
+    }
+
+    refs.divBoxes.lastElementChild.remove();
+  }
 }
 
-function desctroyBoxes() {
-  refs.divEl.innerHTML = "";
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = size + "px";
+    box.style.height = size + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    refs.divBoxes.append(box);
+    size += 10;
+  }
 }
+
+// const refs = {
+//   input: document.querySelector("[type='number']"),
+//   btnCreate: document.querySelector("[data-create]"),
+//   btnDestory: document.querySelector("[data-destroy]"),
+//   div: document.querySelector("#controls"),
+//   divBoxes: document.querySelector("#boxes"),
+// };
+// refs.btnCreate.addEventListener("click", onBtnCreateClick);
+// refs.btnDestory.addEventListener("click", onBtnDestroyClick);
+
+// function onBtnCreateClick() {
+//   let size = 30;
+
+//   const countInput = refs.input.value;
+//   for (let i = 0; i < countInput; i++) {
+//     const box = document.createElement("div");
+//     box.style.width = size + "px";
+//     box.style.height = size + "px";
+//     box.style.backgroundColor = getRandomHexColor();
+//     refs.divBoxes.append(box);
+//     size += 10;
+//   }
+//   refs.input.value = "";
+// }
+
+// function onBtnDestroyClick() {
+//   let countInput = refs.input.value;
+
+//   if (!countInput) {
+//     refs.divBoxes.innerHTML = "";
+//     return;
+//   }
+
+//   const quantity = refs.divBoxes.childElementCount;
+
+//   for (let i = 0; i < countInput; i++) {
+//     if (i > quantity - 1) {
+//       return;
+//     }
+
+//     refs.divBoxes.lastElementChild.remove();
+//   }
+// }
